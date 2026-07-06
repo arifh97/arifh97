9396d0c1d0b5a045a3deee02dd105007bc99a786
arifh97 <arifline16@gmail.com>
2026-07-07 05:53:35 +0600

    release 1.5

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,20 @@ import ProjectsSection from "@/app/components/ProjectsSection";
 import ToolsSection from "@/app/components/ToolsSection";
 import ContactSection from "@/app/components/ContactSection";
 
+import { Analytics } from "@vercel/analytics/next"
+
 export default function Home() {
   return (
-    <main className="min-h-screen overflow-x-hidden">
-      <HeroSection />
-      <AboutSection />
-      <SkillsSection />
-      <ProjectsSection />
-      <ToolsSection />
-      <ContactSection />
-    </main>
+    <>
+      <main className="min-h-screen overflow-x-hidden">
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ToolsSection />
+        <ContactSection />
+      </main>
+      <Analytics />
+    </>
   );
 }
