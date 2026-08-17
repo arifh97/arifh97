7a954234f706c7821f654c2862ad01a17843978d
arifh97 <arifline16@gmail.com>
2026-08-17 10:50:51 +0600

    link updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import Social from './social'
+import { BsWhatsapp } from 'react-icons/bs'
+import { FaTelegramPlane } from "react-icons/fa";
+
 
 export default function Footer() {
+    const social = [
+        {
+            name: 'WhatsApp',
+            path: 'http://wa.me/+8801918599939',
+            icon: <BsWhatsapp />
+        },
+        {
+            name: 'Telegram',
+            path: 'https://t.me/arifh97',
+            icon: <FaTelegramPlane />
+        },
+    ]
     return (
-        <div className="container mx-auto py-5 lg:py-6 border-t border-border text-center">
-            <div className="flex flex-col md:flex-row items-center justify-center gap-4 text-sm text-muted-foreground font-medium">
-                <span>© {new Date().getFullYear()} Arif. All rights reserved.</span>
+        <div className="container mx-auto">
+            <div className="py-5 lg:py-6 border-t border-border text-center">
+                <div className="flex flex-col md:flex-row items-center justify-between gap-4 text-sm text-muted-foreground font-medium">
+                    <span>© {new Date().getFullYear()} Arif. All rights reserved.</span>
+                    <div className="flex items-center gap-x-3">
+                        {social.map((item, idx) => (
+                            <a href={item.path} key={idx} target='_blank' title={item.name} className='flex items-center justify-center rounded-lg size-10 text-lg bg-[#353839] text-white hover:bg-accent transition-all duration-300'>
+                                {item.icon}
+                            </a>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     )
